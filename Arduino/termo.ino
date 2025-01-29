3709df0ba33295b63e86437b1f84e5d5e8095f64
Fabioiscoding <gurjinder07italy@gmail.com>
2025-01-29 13:51:13 +0100

    Update termo.ino

--- Arduino/termo.ino
+++ Arduino/termo.ino
@@ -1,12 +1,12 @@
 #include <DHT11.h>
-
+ 
 DHT11 dht11(2);
-
+ 
 #define LED_ROSSO 8
 #define LED_VERDE 9
-
+ 
 void accendiLed(const int TEMPERATURA, const int SOGLIA_INFERIORE, const int SOGLIA_SUPERIORE){
-  if (TEMPERATURA > SOGLIA_SUPERIORE){
+  if (TEMPERATURA >= SOGLIA_SUPERIORE){
       digitalWrite(LED_ROSSO, HIGH);
       digitalWrite(LED_VERDE, LOW);
     } else if (TEMPERATURA > SOGLIA_INFERIORE){
@@ -17,37 +17,37 @@ void accendiLed(const int TEMPERATURA, const int SOGLIA_INFERIORE, const int SOG
       digitalWrite(LED_VERDE, LOW);
     }
 }
-
+ 
 void setup() {
-    
+   
     pinMode(LED_ROSSO, OUTPUT);
     pinMode(LED_VERDE, OUTPUT);
-
+ 
     Serial.begin(9600);
-  
+ 
     int lettura = 0;
 }
-
+ 
 void loop() {
-
+ 
     //LEGGE LA TEMPERATURA E L'UMIDITA
     int temp = 0;
     int umid = 0;
-
+ 
     // Attempt to read the temperature and humidity values from the DHT11 sensor.
-   int result = dht11.readTemperatureHumidity(temp, umid);
-
+    int result = dht11.readTemperatureHumidity(temp, umid);
+ 
     if (result == 0) {
       String messaggio = String(temp) + " " + String(umid);
         Serial.println(messaggio);
     } else {
         Serial.println(DHT11::getErrorString(result));
     }
-
-
+ 
+ 
     int SOGLIA_SUPERIORE = 27;
-    int SOGLIA_INFERIORE = 25;
-
+    int SOGLIA_INFERIORE = 20;
+ 
     accendiLed(temp, SOGLIA_INFERIORE, SOGLIA_SUPERIORE);
-  
+ 
 }
