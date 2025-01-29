f3d073fdf5b01e3e8b90c5e68a4618a6ef8930e8
Fabioiscoding <gurjinder07italy@gmail.com>
2025-01-29 13:53:02 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,20 +1,50 @@
+import serial
 import dearpygui.dearpygui as dpg
+import multiprocessing as mp
+import imgui
+
+def read_data(q):
+    # crea la seriale
+    SERIALE = "COM5"
+    ser = serial.Serial(SERIALE, 9600)
+
+    while True:
+        # legge i dati (o meglio, legge i byte di dati)
+        data = ser.readline()
+
+        # li converte in una stringa utf-8
+        data = data.decode("utf-8")
+
+        # mette in due variabili, T e H come arduino
+        t, h = data.split(' ')
+
+        # converte in interi
+        h = int(h)
+        t = int(t)
+
+        q.put([h, t])
+
+    ser.close() # chiudi la seriale
 
 
-def save_callback():
-    print("Save Clicked")
 
+if __name__ == '__main__':
+    queue = mp.Queue()
+    read_process = mp.Process(target=read_data, args=(queue,))
+    read_process.start()
 
-dpg.create_context()
-dpg.create_viewport()
-dpg.setup_dearpygui()
+    data = queue.get()
 
-with dpg.window(label="Elaborazione"):
-    dpg.add_text("Hello world")
-    dpg.add_button(label="Save", callback=save_callback)
-    dpg.add_input_text(label="string")
-    dpg.add_slider_float(label="float")
+    #creazione gui
+    dpg.create_context()
+    dpg.create_viewport(title='Custom Title', width=600, height=300)
+    dpg.setup_dearpygui()
 
-dpg.show_viewport()
-dpg.start_dearpygui()
-dpg.destroy_context()
+    with dpg.window(label="Data Reader"):
+        dpg.add_text("Data Reader")
+
+    dpg.show_viewport()
+
+    while dpg.is_dearpygui_running():
+        dpg.render_dearpygui_frame()
+
