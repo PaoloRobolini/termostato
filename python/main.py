7a913a9f557e4e11359cf7b4ff876afd579565fc
SixMax06 <alemilamax@gmail.com>
2025-02-02 11:04:19 +0100

    Updated code
    Code works and data is displayed correctly on the graphs (finally)
    TODO : make the gui prettier

--- python/main.py
+++ python/main.py
@@ -7,7 +7,7 @@ import multiprocessing as mp
 
 def read_data(q):
     # crea la seriale
-    porta_seriale = "COM5"
+    porta_seriale = "COM3"
     ser = serial.Serial(porta_seriale, 9600)
 
     while True:
@@ -35,21 +35,50 @@ if __name__ == '__main__':
     read_process = mp.Process(target=read_data, args=(queue,))
     read_process.start()
 
+    # dichiarazione lista dei dati
+    data = [0, 0]
+    temperature = [0 for i in range(60)]
+    humidity = [0 for i in range(60)]
+    intervals = [i for i in range(60)]
+
     # creazione gui
     dpg.create_context()
     dpg.create_viewport(title='Data Reader')
 
     # aggiunta delle caselle di testo contenenti le varie informazioni
     with dpg.window(label="Data Reader"):
-        dpg.add_text("Data Reader")
+        # aggiunta dati
         dpg.add_text("", tag="data")
+
+        with dpg.plot(label="Temperature Variation", height=400, width=800, tag="temperaturePlot"):
+            # dichiarazione assi
+            dpg.add_plot_axis(dpg.mvXAxis, label="time (s)", tag="timeAxis1")
+            dpg.add_plot_axis(dpg.mvYAxis, label="temperature (C)", tag="temperatureAxis")
+
+            # impostazione limiti degli assi
+            dpg.set_axis_limits("timeAxis1", 0, 60)
+            dpg.set_axis_limits("temperatureAxis", -20, 50)
+
+            # aggiunta delle linee
+            dpg.add_line_series(temperature, intervals, parent="temperatureAxis", tag="temperatureData",
+                                label="Temperature")
+
+        with dpg.plot(label="Humidity Variation", height=400, width=800, tag="humidityPlot"):
+            # dichiarazione assi
+            dpg.add_plot_axis(dpg.mvXAxis, label="time (s)", tag="timeAxis2")
+            dpg.add_plot_axis(dpg.mvYAxis, label="humidity (%)", tag="humidityAxis")
+
+            # impostazione limiti degli assi
+            dpg.set_axis_limits("timeAxis2", 0, 60)
+            dpg.set_axis_limits("humidityAxis", 0, 100)
+
+            # aggiunta delle linee
+            dpg.add_line_series(temperature, intervals, parent="humidityAxis", tag="humidityData",
+                                label="Humidity")
 
     # inizializzazione gui
     dpg.setup_dearpygui()
     dpg.show_viewport()
-
-    # dichiarazione lista dei dati
-    data = [0, 0]
 
     # aggiornamento gui
     while dpg.is_dearpygui_running():
@@ -59,6 +88,17 @@ if __name__ == '__main__':
             except Empty:
                 continue
 
+            temperature.append(data[1])
+            temperature.pop(0)
+
+            humidity.append(data[0])
+            humidity.pop(0)
+
             dpg.set_value("data", data)
+            dpg.set_value('temperatureData', [intervals, temperature])
+            dpg.set_value("humidityData", [intervals, humidity])
 
         dpg.render_dearpygui_frame()
+
+
+    dpg.destroy_context()
