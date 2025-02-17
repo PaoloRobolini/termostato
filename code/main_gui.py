a18ab1421b916aed6625bd2ecb0daa4320756d6b
SixMax06 <alemilamax@gmail.com>
2025-02-17 12:14:58 +0100

    Updated code
    Fixed the exit button

--- code/main_gui.py
+++ code/main_gui.py
@@ -10,11 +10,11 @@ import multiprocessing as mp
 global temperature
 global humidity
 global time
-global colorR
-global colorV
+global color_r
+global color_v
+global read_process
+global queue
 
-colorR = (0,255,0,255)
-colorV = (255,0,0,55)
 
 def read_data(q):
     # dichiarazione della porta seriale
@@ -27,7 +27,7 @@ def read_data(q):
         # converte i bit letti in caratteri utf-8
         numbered_data = serial_data.decode("utf-8")
 
-        # mette i dati in due varabili t (temperatura) ed h (umidità)
+        # mette i dati in due variabili t (temperatura) ed h (umidità)
         t, h = numbered_data.split(' ')
 
         # converte le due variabili in stringhe
@@ -56,12 +56,17 @@ def save_data():
     file_name = Path.cwd().parent / ("logs/" + datetime.now().strftime("%Y-%m-%d-%H%M") + ".json")
     with open(file_name, 'w') as outfile:
         json.dump(data_save, outfile, indent=4, ensure_ascii=False)
+    print("data saved")
 
 
 def exit_program():
     print("exiting program")
+
     dpg.stop_dearpygui()
-    exit()
+    read_process.kill()
+    queue.close()
+
+    print("exited program")
 
 
 if __name__ == '__main__':
@@ -83,6 +88,10 @@ if __name__ == '__main__':
     SOGLIA_INFERIORE = 23
     SOGLIA_SUPERIORE = 27
 
+    # dichiarazione colori per i led
+    color_r = (0, 255, 0, 255)
+    color_v = (255, 0, 0, 55)
+
     # creazione gui
     dpg.create_context()
     dpg.create_viewport(title='Termostato', width=1450, height=950)
@@ -93,7 +102,6 @@ if __name__ == '__main__':
         dpg.add_text("", tag="temperature")
         dpg.add_text("", tag="humidity")
         dpg.add_text("", tag="leds")
-
 
         with dpg.plot(label="Temperature Variation", height=400, width=1000, tag="temperaturePlot"):
             # dichiarazione assi
@@ -121,16 +129,13 @@ if __name__ == '__main__':
             dpg.add_line_series(temperature, intervals, parent="humidityAxis", tag="humidityData",
                                 label="Humidity")
 
-
         # aggiunta bottone di salvataggio
         dpg.add_button(enabled=True, label="Save the last 60 seconds", tag="saveButton",
                        callback=save_data, width=400, height=100, pos=(1012, 78))
         dpg.add_button(enabled=True, label="Exit", tag="exitButton", callback=exit_program, width=400, height=100,
-                            pos=(1012, 182))
-        c1 = dpg.draw_circle(center=(960, 85), radius=8, color=(0, 0, 0, 255), thickness=1, fill=colorV, tag="ledR")
-        c2 = dpg.draw_circle(center=(980, 85), radius=8, color=(0, 0, 0, 255), thickness=1, fill=colorR, tag="ledV")
-
-
+                       pos=(1012, 182))
+        c1 = dpg.draw_circle(center=(960, 85), radius=8, color=(0, 0, 0, 255), thickness=1, fill=color_v, tag="ledR")
+        c2 = dpg.draw_circle(center=(980, 85), radius=8, color=(0, 0, 0, 255), thickness=1, fill=color_r, tag="ledV")
 
     # inizializzazione gui
     dpg.setup_dearpygui()
@@ -154,7 +159,6 @@ if __name__ == '__main__':
             humidity.append(data[0])
             humidity.pop(0)
 
-
             # inserisce il tempo di orologio corrente nella lista
             time.append(datetime.now().strftime("%H:%M:%S:%ms"))
             time.pop(0)
@@ -164,20 +168,19 @@ if __name__ == '__main__':
                 stato_led_verde = True
                 stato_led_rosso = False
                 dpg.configure_item(c1, fill=(0, 255, 0, 255))
-                dpg.configure_item(c2 ,fill=(55,0,0,55))
+                dpg.configure_item(c2, fill=(55, 0, 0, 55))
             elif data[1] >= SOGLIA_SUPERIORE:
                 stato_led_verde = False
                 stato_led_rosso = True
                 dpg.configure_item(c1, fill=(0, 55, 0, 55))
-                dpg.configure_item(c2 ,fill=(255,0,0,255))
+                dpg.configure_item(c2, fill=(255, 0, 0, 255))
             else:
                 stato_led_verde = False
                 stato_led_rosso = False
                 dpg.configure_item(c1, fill=(0, 55, 0, 55))
-                dpg.configure_item(c2 ,fill=(55,0,0,55))
+                dpg.configure_item(c2, fill=(55, 0, 0, 55))
 
             # aggiornamento dati
-
             dpg.set_value("temperature", f"Temperature: {data[1]} °C")
             dpg.set_value("humidity", f"Humidity: {data[0]} %")
             dpg.set_value("leds", f"Led Verde: {stato_led_verde} | Led Rosso: {stato_led_rosso}")
@@ -186,10 +189,7 @@ if __name__ == '__main__':
             dpg.set_value("temperatureData", [intervals, temperature])
             dpg.set_value("humidityData", [intervals, humidity])
 
-
-
-
         # renderizza il frame corrente della gui
         dpg.render_dearpygui_frame()
 
-    dpg.destroy_context()
+    exit()
